506dfb7ac9f7872968e668983f296793df6042db
stellantilet <stellantilet@gmail.com>
2022-03-24 08:16:30 -0400

    add solana yellow in main screen text

--- src/views/Home/Main/Welcome.tsx
+++ src/views/Home/Main/Welcome.tsx
@@ -13,7 +13,7 @@ const Welcome = () => {
       <div className="container mx-auto relative px-4 py-20">
         <h2 className="text-2xl md:text-3xl mb-6">Welcome to the AWPSONClub</h2>
         <div className="font-sans text-lg mb-2">
-          A collection of 555 spaceships on the SOLANA Blockchain
+          A collection of 555 spaceships on the <span className="text-yellow-200">SOLANA</span> Blockchain
         </div>
         <div className="font-sans text-lg mb-4">Explore, Create, Enjoy.</div>
         <a
