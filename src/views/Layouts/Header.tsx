b57f98573e9ea393cd29cc5061f5b0c5cb6b6f0a
stellantilet <stellantilet@gmail.com>
2022-05-06 04:16:01 -0400

    fix(header): fix header in mobileview

--- src/views/Layouts/Header.tsx
+++ src/views/Layouts/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
           "py-4": !isTop,
         })}
       >
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <div className="flex items-center flex-shrink-0 mr-6 text-white">
           <Link href="/">
             <a className="tracking-tight">
               AWPSONClub <span className="hidden sm:inline">x Spaceships</span>
@@ -56,7 +56,7 @@ const Header = () => {
             className="flex items-center px-3 py-2 border hover:bg-gray-800"
           >
             <svg
-              className="fill-current h-3 w-3"
+              className="w-3 h-3 fill-current"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -71,38 +71,40 @@ const Header = () => {
             hidden: !open,
           })}
         >
-          <div className="text-sm lg:flex-grow lg:text-right">
+          <div className="block text-sm lg:flex lg:ml-auto lg:text-right">
             <Link href="https://unkkido.gitbook.io/awpson/">
               <a
-                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mx-4"
+                className="block mt-4 text-teal-200 lg:inline-block lg:mt-0 hover:text-white lg:mx-4"
                 target="_blank"
               >
                 Whitepaper
               </a>
             </Link>
-           
-            <Link href="">
-              <a
-                className="relative inline-block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mx-4 rounded bg-gray-800 hover:bg-gray-700 px-2 py-1"
-                style={{
-                  cursor: `url(${eyesImg.src}), auto`,
-                }}
-              >
-                <div className="absolute font-sans -top-3 -right-4 lowercase px-2 rounded bg-white text-red-400 font-bold">
-                  Soon
-                </div>
-                Staking
-              </a>
-            </Link>
-           <Link href="https://mint.creatormachine.com/awpson">
-              <a
-                className="relative inline-block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mx-4 rounded bg-gray-800 hover:bg-gray-700 px-2 py-1">
-                <div className="absolute font-sans -top-3 -right-4 lowercase px-2 rounded bg-white text-red-400 font-bold">
-                  NOW
-                </div>
-                Mint
-              </a>
-            </Link>
+            <div>
+              <Link href="">
+                <a
+                  className="relative inline-block px-2 py-1 mt-4 text-teal-200 bg-gray-800 rounded lg:inline-block lg:mt-0 hover:text-white lg:mx-4 hover:bg-gray-700"
+                  style={{
+                    cursor: `url(${eyesImg.src}), auto`,
+                  }}
+                >
+                  <div className="absolute px-2 font-sans font-bold text-red-400 lowercase bg-white rounded -top-3 -right-4">
+                    Soon
+                  </div>
+                  Staking
+                </a>
+              </Link>
+            </div>
+            <div>
+              <Link href="https://mint.creatormachine.com/awpson">
+                <a className="relative inline-block px-2 py-1 mt-4 text-teal-200 bg-gray-800 rounded lg:inline-block lg:mt-0 hover:text-white lg:mx-4 hover:bg-gray-700">
+                  <div className="absolute px-2 font-sans font-bold text-red-400 lowercase bg-white rounded -top-3 -right-4">
+                    NOW
+                  </div>
+                  Mint
+                </a>
+              </Link>
+            </div>
           </div>
           <div className="flex gap-6 text-3xl lg:ml-4">
             <a
